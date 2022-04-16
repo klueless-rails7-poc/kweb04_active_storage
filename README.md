0fbd33b5277c78fc05876fda071a975219b43e14
David Cruwys <david@ideasmen.com.au>
2022-04-17 09:02:46 +1000

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # README
 
+
+[Active Storage For Image Uploads](https://www.youtube.com/watch?v=1cw6qO1EYGw&list=PL3mtAHT_eRewPIrCD1Z7eXyx5fJ3E-Vnn&index=1+)
+
+```bash
+rails active_storage:install 
+rails action_text:install
+
+```
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
---
+++ app/models/pin.rb
@@ -0,0 +1,25 @@
+class Pin < ApplicationRecord
+  has_one_attached :image
+  has_many_attached :pictures
+  has_rich_text :body
+
+  def viewable?
+    image.content_type.in?(%w(image/jpeg image/png))
+  end
+
+  def image_as_thumbnail
+    return unless image.content_type.in?(%w(image/jpeg image/png))
+  
+    image.variant(resize_to_limit: [300, 300]).processed
+  end
+
+  def pictures_as_thumbnails
+    pictures.map do |picture|
+      picture.variant(resize_to_limit: [150, 150]).processed
+    end
+  end
+
+  def picture_as_thumbnail(picture)
+    picture.variant(resize_to_limit: [200, 200]).processed
+  end
+end
